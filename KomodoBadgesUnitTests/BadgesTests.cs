6496a419768869ee9421f3e932f3f3a066d6cde5
Kevin Blanchard <kevindanielblanchard@gmail.com>
2021-07-15 23:11:03 -0400

    Modified unit test projects

---
+++ KomodoBadgesUnitTests/BadgesTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KomodoBadgesApp;
+
+namespace KomodoBadgesApp
+{
+    [TestClass]
+    public class BadgesTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
